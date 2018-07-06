c503f8f59cc70780560bb72e579898a1557703e0
Greg Cabrera <gregcabrera@MacBooks-MacBook-Pro-2.local>
2018-07-06 15:07:56 +0800

    added button component

--- app/src/main/java/com/gospce/adc/gospcelib/CustomButton.java
+++ app/src/main/java/com/gospce/adc/gospcelib/CustomButton.java
@@ -1,6 +1,8 @@
 package com.gospce.adc.gospcelib;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
 
@@ -8,14 +10,28 @@ public class CustomButton extends AppCompatButton {
 
     public CustomButton(Context context) {
         super(context);
+
     }
 
     public CustomButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(attrs);
     }
 
     public CustomButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(attrs);
     }
+
+    public void init(AttributeSet attrs){
+        TypedArray attributes = getContext().obtainStyledAttributes(attrs,R.styleable.CustomButton);
+        setText(attributes.getString(R.styleable.CustomButton_buttonText));
+        setBackgroundResource(R.drawable.custom_button);
+
+        GradientDrawable bgShape = (GradientDrawable)getBackground();
+        bgShape.mutate();
+        bgShape.setColor(attributes.getColor(R.styleable.CustomButton_buttonColor, 0));
+    }
+
 
 }
--- app/src/main/java/com/gospce/adc/gospcelib/MainActivity.java
+++ app/src/main/java/com/gospce/adc/gospcelib/MainActivity.java
@@ -1,13 +1,25 @@
 package com.gospce.adc.gospcelib;
 
+import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CustomButton myButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = findViewById(R.id.myButton);
+
+        myButton.setOnClickListener(new CustomButton.OnClickListener(){
+            public void onClick(View v){
+                System.out.println("test click");
+            }
+        });
     }
 }
